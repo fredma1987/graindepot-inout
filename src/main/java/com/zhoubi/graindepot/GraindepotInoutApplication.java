221773a35260bbd1a4a48d11ada91c1c511ad9f8
familyProject <963600060@qq.com>
2018-12-29 00:00:31 +0800

    本地提交

--- src/main/java/com/zhoubi/graindepot/GraindepotInoutApplication.java
+++ src/main/java/com/zhoubi/graindepot/GraindepotInoutApplication.java
@@ -3,12 +3,16 @@ package com.zhoubi.graindepot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.session.data.redis.RedisFlushMode;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
+@EnableRedisHttpSession(redisFlushMode = RedisFlushMode.IMMEDIATE)
 public class GraindepotInoutApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
