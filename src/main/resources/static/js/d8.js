fa57cdde56e1906f8693edb5c0e076b96cb02cf8
fredma1987 <356803326@qq.com>
2019-01-03 22:36:28 +0800

    IC卡读写

--- src/main/resources/static/js/d8.js
+++ src/main/resources/static/js/d8.js
@@ -20,7 +20,7 @@ function m1CardTest()
     st = rd.dc_init(100, 115200);
     if(st <= 0)
     {
-        msg=("初始化通讯口失败!");
+        msg="初始化通讯口失败!";
         alert(msg);
         return;
     }
@@ -33,28 +33,30 @@ function m1CardTest()
      st = rd.dc_card_double(0);
     if(st != 0)
     {
-        msg=("寻卡异常!");
+        msg="寻卡异常!";
         rd.dc_exit();
         alert(msg);
         return;
     }
     var cardid=rd.get_bstrRBuffer_asc;
-
+    /*在第一个扇区读写*/
     rd.put_bstrSBuffer_asc = "FFFFFFFFFFFF";
-    /*功 能：将密码装入读写模块RAM中*/
+    /*功 能：将密码装入读写模块RAM中 ，第二个参数指扇区*/
     st = rd.dc_load_key(0, 0);
     if(st != 0)
     {
-        msg=("密码装入异常!");
+        msg="密码装入异常!";
         rd.dc_exit();
+        alert(msg);
         return;
     }
-    /*功 能：核对密码函数*/
+    /*功 能：核对密码函数，，第二个参数指扇区*/
     st = rd.dc_authentication(0, 0);
     if(st != 0)
     {
-        msg=("核对密码异常!");
+        msg="核对密码异常!";
         rd.dc_exit();
+        alert(msg);
         return;
     }
     st=rd.dc_read(1);
@@ -62,40 +64,100 @@ function m1CardTest()
     if(true){//判断卡是否绑定中
         /*后面要改成动态获取单据号*/
         var billno="2019-01-02-0001";
-        var billno_buf="";
-        /*转成16进制字节流*/
-        for(var i=0;i<billno.length;i++){
-            billno_buf+=billno.charCodeAt(i).toString(16);
-        }
-        rd.put_bstrSBuffer_asc = billno_buf;
+        rd.put_bstrSBuffer = billno;
         /**
          * 功 能：向卡中写入数据,参数：表示第几块，从0开始，0块为卡号
          */
         st = rd.dc_write(1);
         if(st != 0)
         {
-            msg=("写卡异常!");
+            msg="写卡异常!";
             rd.dc_exit();
+            alert(msg);
             return;
         }
         /*后面要改成获取用户信息*/
         var userinfo="马恩恩";
-        var userinfo_buf="";
-        /*转成16进制字节流*/
-        for(var i=0;i<userinfo.length;i++){
-            userinfo_buf+=userinfo.charCodeAt(i).toString(16);
-        }
-        rd.put_bstrSBuffer_asc = userinfo_buf;
+        rd.put_bstrSBuffer = userinfo;
         /**
          * 功 能：向卡中写入数据,参数：表示第几块，从0开始，0块为卡号
          */
-        st = rd.dc_write(2);
-        rd.dc_read(2);
-        alert(rd.get_bstrRBuffer);
+        try{
+            st = rd.dc_write(2);
+        }catch (e){
+            alert(e);
+            rd.dc_exit();
+            return;
+        }
         if(st != 0)
         {
-            msg=("写卡异常!");
+            msg="写卡异常!";
             rd.dc_exit();
+            alert(msg);
+            return;
+        }
+
+        /*在第二个扇区读写*/
+        rd.put_bstrSBuffer_asc = "FFFFFFFFFFFF";
+        /*功 能：将密码装入读写模块RAM中，，第二个参数指扇区*/
+        st = rd.dc_load_key(0, 1);
+        if(st != 0)
+        {
+            msg="密码装入异常!";
+            rd.dc_exit();
+            alert(msg);
+            return;
+        }
+        /*功 能：核对密码函数，，第二个参数指扇区*/
+        st = rd.dc_authentication(0, 1);
+        if(st != 0)
+        {
+            msg="核对密码异常!";
+            rd.dc_exit();
+            alert(msg);
+            return;
+        }
+
+        /*后面要改成获取车辆信息*/
+        var carinfo="苏BA3G19|";
+        rd.put_bstrSBuffer = carinfo;
+        /**
+         * 功 能：向卡中写入数据,参数：表示第几块，从0开始，0块为卡号
+         */
+        try{
+            st = rd.dc_write(4);
+        }catch (e){
+            alert(e);
+            rd.dc_exit();
+            return;
+        }
+        if(st != 0)
+        {
+            msg="写卡异常!";
+            rd.dc_exit();
+            alert(msg);
+            return;
+        }
+        /*后面要改成获取身份证信息*/
+        var idinfo="320722198704035496";
+        rd.put_bstrSBuffer = idinfo.substr(0,16);
+        /**
+         * 功 能：向卡中写入数据,参数：表示第几块，从0开始，0块为卡号
+         */
+        try{
+            st = rd.dc_write(5);
+            rd.put_bstrSBuffer = idinfo.substr(16,18)+"|";
+            st = rd.dc_write(6);
+        }catch (e){
+            alert(e);
+            rd.dc_exit();
+            return;
+        }
+        if(st != 0)
+        {
+            msg="写卡异常!";
+            rd.dc_exit();
+            alert(msg);
             return;
         }
     }else{
@@ -114,73 +176,37 @@ function m1CardTest()
     //rd.get_bstrRBuffer_asc;
 
     /*功 能：蜂鸣 ,参数：蜂鸣时间 单位毫秒*/
-    st = rd.dc_beep(50);
+    st = rd.dc_beep(20);
+    st = rd.dc_beep(20);
     /*功 能：关闭端口*/
     st = rd.dc_exit();
 }
-function encode64(input) {
-    input = escape(input);
-    var output = "";
-    var chr1, chr2, chr3 = "";
-    var enc1, enc2, enc3, enc4 = "";
-    var i = 0;
-    do {
-        chr1 = input.charCodeAt(i++);
-        chr2 = input.charCodeAt(i++);
-        chr3 = input.charCodeAt(i++);
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
+function stringToByte(str) {
+    var bytes = new Array();
+    var len, c;
+    len = str.length;
+    for(var i = 0; i < len; i++) {
+        c = str.charCodeAt(i);
+        if(c >= 0x010000 && c <= 0x10FFFF) {
+            bytes.push(((c >> 18) & 0x07) | 0xF0);
+            bytes.push(((c >> 12) & 0x3F) | 0x80);
+            bytes.push(((c >> 6) & 0x3F) | 0x80);
+            bytes.push((c & 0x3F) | 0x80);
+        } else if(c >= 0x000800 && c <= 0x00FFFF) {
+            bytes.push(((c >> 12) & 0x0F) | 0xE0);
+            bytes.push(((c >> 6) & 0x3F) | 0x80);
+            bytes.push((c & 0x3F) | 0x80);
+        } else if(c >= 0x000080 && c <= 0x0007FF) {
+            bytes.push(((c >> 6) & 0x1F) | 0xC0);
+            bytes.push((c & 0x3F) | 0x80);
+        } else {
+            bytes.push(c & 0xFF);
         }
-        output = output +
-            keyStr.charAt(enc1) +
-            keyStr.charAt(enc2) +
-            keyStr.charAt(enc3) +
-            keyStr.charAt(enc4);
-        chr1 = chr2 = chr3 = "";
-        enc1 = enc2 = enc3 = enc4 = "";
-    } while (i < input.length);
-    return (output);
-}
-function decode64(input) {
-    var output = "";
-    var chr1, chr2, chr3 = "";
-    var enc1, enc2, enc3, enc4 = "";
-    var i = 0;
-    // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-    var base64test = /[^A-Za-z0-9\+\/\=]/g;
-    if (base64test.exec(input)) {
-        alert("There were invalid base64 characters in the input text.\n" +
-            "Valid base64 characters are A-Z, a-z, 0-9, '+', '/', and '='\n" +
-            "Expect errors in decoding.");
     }
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-    do {
-        enc1 = keyStr.indexOf(input.charAt(i++));
-        enc2 = keyStr.indexOf(input.charAt(i++));
-        enc3 = keyStr.indexOf(input.charAt(i++));
-        enc4 = keyStr.indexOf(input.charAt(i++));
-        chr1 = (enc1 << 2) | (enc2 >> 4);
-        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
-        output = output + String.fromCharCode(chr1);
-        if (enc3 != 64) {
-            output = output + String.fromCharCode(chr2);
-        }
-        if (enc4 != 64) {
-            output = output + String.fromCharCode(chr3);
-        }
-        chr1 = chr2 = chr3 = "";
-        enc1 = enc2 = enc3 = enc4 = "";
-    } while (i < input.length);
-    return (output);
-}
+    return bytes;
 
+
+}
 function typeATest()
 {
     var st;
