2bda897c8944db592fe4b647a9b3a48c1713bab8
fredma1987 <356803326@qq.com>
2019-01-31 15:37:09 +0800

    称重

--- src/main/java/com/zhoubi/graindepot/controller/OnstorageController.java
+++ src/main/java/com/zhoubi/graindepot/controller/OnstorageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("onstorage")
-public class Onstorage extends BaseController{
+public class OnstorageController extends BaseController{
     @GetMapping("toInitOnstorage")
     public String toInitOnstorage(){
         return "initonstorage";
--- src/main/java/com/zhoubi/graindepot/controller/WeightController.java
+++ src/main/java/com/zhoubi/graindepot/controller/WeightController.java
@@ -1,11 +1,16 @@
 package com.zhoubi.graindepot.controller;
 
 
+import com.zhoubi.graindepot.base.PagerModel;
+import com.zhoubi.graindepot.bean.BaseUser;
+import com.zhoubi.graindepot.bean.Inout;
+import com.zhoubi.graindepot.bean.InoutBean;
+import com.zhoubi.graindepot.biz.InoutBiz;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +21,8 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("weight")
 public class WeightController extends BaseController{
+    @Autowired
+    private InoutBiz inoutBiz;
     @ModelAttribute("ctx")
     public void getAccount(Model model,HttpServletRequest request) {
         String ctx = request.getContextPath();
@@ -26,5 +33,19 @@ public class WeightController extends BaseController{
     @RequestMapping(value="/toWeight",method = RequestMethod.GET)
     public String toRegister(Model model, HttpServletRequest request, HttpServletResponse response){
         return "weight";
+    }
+    @GetMapping("newPageList")
+    @ResponseBody
+    public PagerModel newPageList(int start, int length) {
+        BaseUser currentUser = getCurrentUser();
+        PagerModel<Inout> e=new PagerModel();
+        e.addOrder("billcode desc");
+        e.setStart(start);
+        e.setLength(length);
+        if(currentUser.getGraindepotid()!=0) {
+            e.putWhere("graindepotid", currentUser.getGraindepotid());
+        }
+        PagerModel<Inout> result=inoutBiz.selectListByPage(e);
+        return result;
     }
 }
