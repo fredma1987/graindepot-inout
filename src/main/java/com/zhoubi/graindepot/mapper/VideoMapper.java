459e1b6c1879db4f81a123671553091f3976488f
fredma1987 <356803326@qq.com>
2019-02-13 09:49:19 +0800

    出入库代码

---
+++ src/main/java/com/zhoubi/graindepot/mapper/VideoMapper.java
@@ -0,0 +1,1 @@
+package com.zhoubi.graindepot.mapper;import com.zhoubi.graindepot.bean.Video;import com.zhoubi.graindepot.base.BaseMapper;public interface VideoMapper extends BaseMapper<Video> {}
